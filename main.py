c5b502e40d0a6a24063c2f9685fa9ad9168316ec
Aleksandr Shcherbakov <djafar1001@yandex.ru>
2023-06-26 18:25:34 +0300

    Создание приветственного блока

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import HR_Lib
 import telebot
 from telebot import types
 from os import path
+
 import pickle
 from datetime import datetime
 
@@ -25,8 +26,8 @@ class Employee:
 
 
 # Открываем или создаем словарь для хранения данных о сотрудниках
-if path.exists('employees.dt'):
-    with open('employees.dt', 'rb', encoding='utf-8') as file:
+if path.exists('employees.db'):
+    with open('employees.db', 'rb') as file:
         employees = pickle.load(file)
 else:
     employees = {}
@@ -34,27 +35,58 @@ else:
 
 @bot.message_handler(commands=['start'])
 def handle_start(message, employee=None):
-    # Создание объекта сотрудника и сохранение его в словаре по идентификатору чата
+    """
+    После ввода команды /start Проверяем наличие пользователя в словаре сотрудников
+    и в случае отсутствия запрашиваем его имя. Полученный результат передаем
+    в функцию start_dialog
+
+    :param message:
+    :return:
+    """
     if message.chat.id not in employees.keys():
         bot.send_message(message.chat.id, 'Доброе утро! Я Вас приветствую в Банке России')
         # Запрос имени сотрудника
-        bot.send_message(message.chat.id, "Пожалуйста, укажите, как к Вам можно обращаться:")
-
-        employee = Employee(message.text)
-        employees[message.chat.id] = employee
-        bot .send_message(message.chat.id, f'{employee.name} с началом трудовой деятельности в Банке Росси')
+        answer = bot.send_message(message.chat.id, "Пожалуйста, укажите, как к Вам можно обращаться:")
+        bot.register_next_step_handler(answer, start_dialog)
     else:
-        ...
-
-        bot .send_message(message.chat.id, f'{employee.name} Вы готовы приступить к работе по адаптации?')
-
-
-
-
+        if not all(employees[message.chat.id].courses_completed):
+            bot.send_message(message.chat.id, 'Давайте продолжим')
+        else:
+            bot.send_message(message.chat.id, 'Вы успешно прошли все этапы адаптации'
+                                              '\nУдачи в работе')
 
 
 
 
+def start_dialog(message):
+    """
+    Создание объекта сотрудника и сохранение его в словаре по
+    идентификатору чата с последующей сериализацией в битовый файл/
+    Выполняем диалог первого дня
+
+    :param message:
+    :return:
+    """
+    employee = Employee(message.text)
+    employees[message.chat.id] = employee
+
+    with open('employees.db', 'wb') as file:
+        pickle.dump(employees, file)
+
+    bot.send_message(message.chat.id, f'{employee.name} с началом трудовой деятельности в Банке Росси')
+
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    butt_yes = types.InlineKeyboardButton('Да', callback_data='1')
+    butt_no = types.InlineKeyboardButton('Нет', callback_data='0')
+    markup.add(butt_yes, butt_no)
+
+    bot.send_message(message.chat.id,
+                     f'{employee.name} Вы готовы приступить к работе по адаптации?',
+                     reply_markup=markup)
+
+# @bot.callback_query_handlers(func=lambda call: True):
+# def reaktion(call):
+#     pass
 
 
 if __name__ == '__main__':
@@ -63,6 +95,5 @@ if __name__ == '__main__':
     # Запуск бота в работу на ожидание сообщений в бесконечном режиме без интервалов
     print('Started')
     bot.polling(none_stop=True, interval=0)
-    with open('employees.dt', 'wb', encoding='utf-8') as file:
-        pickle.dump(employees)
+
     print('Stoped')
