32fb53b9c6ae29c716eeb167e9d81d2b9d5ba9ec
Aleksandr Shcherbakov <djafar1001@yandex.ru>
2023-06-28 19:13:42 +0300

    Разработка рекций на callback запросы и реакция на reply-меню

--- HR_Lib/__init__.py
+++ HR_Lib/__init__.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from telebot import types
 
 
 def format_time(timestamp: int) -> str:
@@ -11,3 +12,28 @@ def format_time(timestamp: int) -> str:
     df_time = f'{dt_object:%d-%m-%Y %H:%M:%S}'
 
     return df_time
+
+
+def simple_menu():
+    """
+    Функция определения Inline-меню с двумя кнопками "Да" и "Нет"
+    :return: markup: InlineKeyboard
+    """
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    butt_yes = types.InlineKeyboardButton('Да', callback_data='1')
+    butt_no = types.InlineKeyboardButton('Нет', callback_data='0')
+    markup.add(butt_yes, butt_no)
+
+    return markup
+
+
+def menu_ready():
+    """
+    Функция организации Reply-меню для завершения этапа прохождения адаптации
+    :return: kb:ReplyKeyboard
+    """
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn_1 = types.KeyboardButton(text='Прошел✔️')
+    kb.add(btn_1)
+
+    return kb
--- main.py
+++ main.py
@@ -3,25 +3,27 @@ HRБот– виртуальный ассистент предназначенн
 с принятыми в Банк России на работу сотрудниками,
 обученный общаться по ограниченному кругу сценариев
 """
-from setings_HR import HR_BOT_TOKEN as TOKEN
-import HR_Lib
+from setings_HR import HR_BOT_TOKEN as TOKEN, BOT_MESSSAGE as mess
+import HR_Lib as lib
 
 import telebot
 from telebot import types
-from os import path
+from os import path, remove
 
 import pickle
-from datetime import datetime
+from time import time, sleep
 
 # Создание экземпляра бота с использованием токена
-bot = telebot.TeleBot(TOKEN.get('token'))  # привязка бота к коду
+bot = telebot.TeleBot(TOKEN['token'])  # привязка бота к коду
 
 
 # Класс, представляющий нового сотрудника
 class Employee:
     def __init__(self, name):
         self.name = name
-        self.training_start = datetime.now()
+#        self.training_start = datetime.now()
+        self.training_start = time()
+        self.current_course = 0
         self.courses_completed = [0] * 5
 
 
@@ -44,9 +46,9 @@ def handle_start(message, employee=None):
     :return:
     """
     if message.chat.id not in employees.keys():
-        bot.send_message(message.chat.id, 'Доброе утро! Я Вас приветствую в Банке России')
+        bot.send_message(message.chat.id, mess[0][0])
         # Запрос имени сотрудника
-        answer = bot.send_message(message.chat.id, "Пожалуйста, укажите, как к Вам можно обращаться:")
+        answer = bot.send_message(message.chat.id, mess[0][1])
         bot.register_next_step_handler(answer, start_dialog)
     else:
         if not all(employees[message.chat.id].courses_completed):
@@ -54,8 +56,6 @@ def handle_start(message, employee=None):
         else:
             bot.send_message(message.chat.id, 'Вы успешно прошли все этапы адаптации'
                                               '\nУдачи в работе')
-
-
 
 
 def start_dialog(message):
@@ -73,31 +73,53 @@ def start_dialog(message):
     with open('employees.db', 'wb') as file:
         pickle.dump(employees, file)
 
-    bot.send_message(message.chat.id, f'{employee.name} с началом трудовой деятельности в Банке Росси')
+    bot.send_message(message.chat.id, f'Рад знакомству {employee.name}!\n{mess[1][0]}!')
+    #    bot.send_message(message.chat.id, f'{employee.name} с началом трудовой деятельности в Банке Росси')
 
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    butt_yes = types.InlineKeyboardButton('Да', callback_data='1')
-    butt_no = types.InlineKeyboardButton('Нет', callback_data='0')
-    markup.add(butt_yes, butt_no)
+    # markup = types.InlineKeyboardMarkup(row_width=2)
+    # butt_yes = types.InlineKeyboardButton('Да', callback_data='1')
+    # butt_no = types.InlineKeyboardButton('Нет', callback_data='0')
+    # markup.add(butt_yes, butt_no)
 
+    # bot.send_message(message.chat.id,
+    #                  f'{employee.name} Вы готовы приступить к работе по адаптации?',
+    #                  reply_markup=lib.simple_menu())
     bot.send_message(message.chat.id,
-                     f'{employee.name} Вы готовы приступить к работе по адаптации?',
-                     reply_markup=markup)
+                     mess[1][1],
+                     reply_markup=lib.simple_menu())
 
     @bot.callback_query_handler(func=lambda call: True)
-    def reaktion(call):
+    def pressing_reaction(call):
         if call.data == '1':
             bot.edit_message_text(chat_id=call.message.chat.id,
                                   message_id=call.message.id,
-                                  text=f'{employee.name} сегодня в рамках программы адаптации\n'
-                                       f'Вам необходимо пройти вводный курс\n'
-                                       f'"Добро пожаловать в Банк России" ')
-        pass
+                                  text=f'{employee.name}  {mess[1][2]}')
+            bot.send_message(call.message.chat.id, mess[1][3],
+                             reply_markup=lib.menu_ready())
+        else:
+            ...
+
+@bot.message_handler(content_types=['text'])
+def text_reaction(message):
+    if message.text == 'удалить':
+        path_file = 'employees.db'
+        try:
+            remove(path_file)
+        except:
+            bot.send_message(message.chat.id,f'Файл {path_file} не найден')
+    elif message.text == 'Прошел✔️':
+
+        employees[message.chat.id].courses_completed[employees[message.chat.id].current_course] = 1
+        employees[message.chat.id].current_course += 1
+
+        bot.send_message(message.chat.id, mess[1][4],
+                         reply_markup=lib.simple_menu())
+
 
 
 if __name__ == '__main__':
     print('Мой HR-бот')
-    print(HR_Lib.format_time(4567824))
+    print(lib.format_time(time()))
     # Запуск бота в работу на ожидание сообщений в бесконечном режиме без интервалов
     print('Started')
     bot.polling(none_stop=True, interval=0)
